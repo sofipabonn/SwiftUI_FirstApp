850a9d97ea5f048d53f490d8fc895b50729e0052
sofipabonn <sofip1012@gmail.com>
2026-03-30 13:09:26 -0500

    Add files via upload

---
+++ FirstSwiftUI/FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Pabon Silva, Sofia on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
